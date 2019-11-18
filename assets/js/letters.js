4a24b1ad30d34f9697fdaed085c99dbcf0c2d972
Lisa <lisa.michael@hotmail.com>
2019-11-17 20:10:46 -0500

    added code for contact form

--- assets/js/letters.js
+++ assets/js/letters.js
@@ -39,4 +39,67 @@ anime.timeline({loop: true})
     delay: 1000
   });
 
+
+
+  // adding contact form to my database: 
+
+   // test database created in class 
+  var firebaseConfig = {
+    apiKey: "AIzaSyC4N5iN40dKR8LNLwBw63QawtIxptSQQec",
+    authDomain: "lisa-test-oct24.firebaseapp.com",
+    databaseURL: "https://lisa-test-oct24.firebaseio.com",
+    projectId: "lisa-test-oct24",
+    storageBucket: "lisa-test-oct24.appspot.com",
+    messagingSenderId: "88210040314",
+    appId: "1:88210040314:web:0f83a6c9e1f5f9032a3043",
+    measurementId: "G-3WC6LSLK5T"
+  };
+
+
+ 
+
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+
+  
+
+  // button for adding train schedule
+  $('#add-btn').on('click', function () {
+
+    event.preventDefault();
+
+    // Grab user input 
+
+    let tmpName = $('#name-input').val().trim();
+    let tmpemail = $('#email-input').val().trim();
+    let tmpsubject = $('#subject').val().trim();
+    let tmpmessage = $('#message').val().trim();
+
+    // no fields in input form should be blank
+    
+   // else {
+
+      // create temporary object for holding train data
+      let tempContact = {
+        Name: tmpName,
+        email: tmpemail,
+        subject: tmpsubject,
+        message: tmpmessage,
+
+      };  // end of newTrain input
+
+
+
+      database.ref().push(tempContact)
+
+      // clear all of the text boxes 
+      $("#name").val("");
+      $("#email").val("");
+      $("#subject").val("");
+      $("#message").val("");
+   // }
+
+  }); // end of submit form 
+
+
 })
